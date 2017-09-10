94c0d7a1936b6d58af900c02dc93a6c4a6c43985
Henrikas Kuzmickas <henry.kuzmick@gmail.com>
2017-09-10 13:08:53 +0200

    Create topics app

---
+++ discuss/config/config.exs
@@ -0,0 +1,33 @@
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Mix.Config module.
+#
+# This configuration file is loaded before any dependency and
+# is restricted to this project.
+use Mix.Config
+
+# General application configuration
+config :discuss,
+  ecto_repos: [Discuss.Repo]
+
+# Configures the endpoint
+config :discuss, Discuss.Endpoint,
+  url: [host: "localhost"],
+  secret_key_base: "RYIPE9XundvysH51/e/vS7bWr8NtbqefslVNNwIDIV5a5SCUe0RPODzRQ2W7paJm",
+  render_errors: [view: Discuss.ErrorView, accepts: ~w(html json)],
+  pubsub: [name: Discuss.PubSub,
+           adapter: Phoenix.PubSub.PG2]
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+
+config :ueberauth, Ueberauth,
+  providers: [
+    github: { Ueberauth.Strategy.Github, [] }
+  ]
+
+import_config "#{Mix.env}.exs"
